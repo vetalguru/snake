3e7344e09cd81c448774ce0271a7e0a03934e0f1
Vitalii Shkibtan <vetalguru@gmail.com>
2022-03-25 00:30:23 +0200

    Set text for help dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -28,7 +28,13 @@ MainWindow::~MainWindow() {
 
 void MainWindow::aboutDialog() {
     QMessageBox msg;
-    msg.setText("Snake");
+    msg.setText("The game involves controlling a single block or snakehead"
+                "by turning only left or right by ninety degrees until you"
+                "manage to eat an apple. When you get the apple, the Snake"
+                "grows an extra block or body segment."
+                "If, or rather when, the snake bumps into the edge of the"
+                "screen or accidentally eats himself the game is over. The"
+                "more apples the snake eats the higher the score.");
     msg.exec();
 }
 
